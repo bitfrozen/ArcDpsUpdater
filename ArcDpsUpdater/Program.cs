bb2937727a55f7e99df989254089e6f36ca52da6
Ryan Whitmire <ryanw51@gmail.com>
2018-01-13 11:00:55 -0500

    remove TopShelf

--- ArcDpsUpdater/Program.cs
+++ ArcDpsUpdater/Program.cs
@@ -1,29 +1,58 @@
 ﻿using System;
-using Topshelf;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ArcDpsUpdater
 {
     internal class Program
     {
+        private const string hashPath = @"C:\Program Files\Guild Wars 2\bin64\d3d9.dll.md5sum";
+        private const string dllPath = @"C:\Program Files\Guild Wars 2\bin64\d3d9.dll";
+        private const string dllUri = "https://www.deltaconnected.com/arcdps/x64/d3d9.dll";
+        private const string hashUri = "https://www.deltaconnected.com/arcdps/x64/d3d9.dll.md5sum";
+        private const string gameExe = @"C:\Program Files\Guild Wars 2\Gw2-64.exe";
+        private static readonly HttpClient httpClient = new HttpClient();
+
         private static void Main()
         {
-            var rc = HostFactory.Run(x =>
+            RunAsync().Wait();
+            Process.Start(gameExe);
+        }
+
+        private static async Task RunAsync()
+        {
+            Console.WriteLine("checking for ArcDps updates");
+
+            var serverHash = await GetServerHashAsync();
+            var localHash = GetLocalHash();
+
+            if (serverHash != localHash)
             {
-                x.Service<Updater>(s =>
-                {
-                    s.ConstructUsing(name => new Updater());
-                    s.WhenStarted(updater => updater.Start());
-                    s.WhenStopped(updater => updater.Stop());
-                });
+                Console.WriteLine("updating ArcDps");
+                await UpdateAsync(serverHash);
+                Console.WriteLine("ArcDps update complete");
+            }
+        }
 
-                x.RunAsLocalSystem();
-                x.SetDescription("Automatic updater for Guild Wars 2 ArcDps plugin");
-                x.SetDisplayName("ArcDps Updater");
-                x.SetServiceName("ArcDpsUpdater");
-            });
+        private static string GetLocalHash()
+        {
+            return File.Exists(hashPath) ? File.ReadAllText(hashPath) : string.Empty;
+        }
 
-            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());
-            Environment.ExitCode = exitCode;
+        private static async Task UpdateAsync(string serverHash)
+        {
+            var response = await httpClient.GetAsync(dllUri);
+            var bytes = await response.Content.ReadAsByteArrayAsync();
+            File.WriteAllBytes(dllPath, bytes);
+            File.WriteAllText(hashPath, serverHash);
+        }
+
+        private static async Task<string> GetServerHashAsync()
+        {
+            var response = await httpClient.GetAsync(hashUri);
+            return await response.Content.ReadAsStringAsync();
         }
     }
 }
--- README.md
+++
@@ -1,6 +0,0 @@
-## Installation
-
-1. Build ArcDpsUpdater for Release
-2. Run cmd as Administrator
-3. `bin\Release\ArcDpsUpdater.exe install`
-4. `net start ArcDpsUpdater`
